5d98e5e23f006a1dcc2ee05722e6842efbe6a4e5
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-07 00:45:13 +0530

    authcontext

---
+++ client/src/contexts/AuthContext.js
@@ -0,0 +1,34 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("iao-token");
+    const userData = localStorage.getItem("iao-user");
+    if (token && user) {
+      setUser(JSON.parse(userData));
+    }
+    setLoading(false);
+  }, []);
+
+  const login = (userData, token) => {
+    localStorage.setItem("iao-token", token);
+    localStorage.setItem("iao-user", user, JSON.stringify(userData));
+  };
+
+  const logout = (userData, token) => {
+    localStorage.removeItem("iao-token");
+    localStorage.removeItem("iao-user");
+  };
+  return (
+    <AuthContext.Provider value={{ user, login, logout, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
