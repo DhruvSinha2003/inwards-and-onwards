d3ec58ca96639b586da8196d584a19b1c529b2a0
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-07 00:09:39 +0530

    user model

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -7,3 +7,67 @@ const generateToken = (userId) => {
     expiresIn: "7d",
   });
 };
+
+exports.register = async (req, res) => {
+  try {
+    const { email, password, username } = req.body;
+
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.statue(400).json({ message: "User already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    user = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await user.save();
+
+    const token = generateToken(user._id);
+
+    res.status(201).json({
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = generateToken(user._id);
+
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
