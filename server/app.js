e5e7d4d5e5802772ee08bd8d7d4920a015fd4d64
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-07 00:17:00 +0530

    auth implemented

--- server/app.js
+++ server/app.js
@@ -30,3 +30,18 @@ app.use((err, req, res, next) => {
     error: process.env.NODE_ENV === "development" ? err.message : undefined,
   });
 });
+
+const authRoutes = require("./routes/auth");
+
+app.use("/api/auth", authRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not Found" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
