c85f153c69a4a6236a77b1a37608e68b8fa6b2e2
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-07 00:13:27 +0530

    auth routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,2 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const { register, login } = require("../controllers/authController");
+const { body } = require("express-validator");
+
+router.post(
+  "/register",
+  [
+    body("email").isEmail(),
+    body("password").isLength({ min: 6 }),
+    body("username").isLength({ min: 3 }),
+  ],
+  register
+);
+
+router.post(
+  "/login",
+  [body("email").isEmail, body("password").exists()],
+  login
+);
+
+module.exports = router;
