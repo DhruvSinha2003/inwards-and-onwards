6255d55a82fda0dda904c4c1c82fec7c3e45e9b5
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-09 16:53:56 +0530

    profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import { useTheme } from "../contexts/ThemeContext";
 
 const Profile = () => {
+  const { theme } = useTheme();
   const navigate = useNavigate();
 
   const handleBackClick = () => {
@@ -9,9 +12,20 @@ const Profile = () => {
   };
 
   return (
-    <div>
-      <button onClick={handleBackClick}>Back</button>
-      <h1>Profile Page</h1>
+    <div
+      className="min-h-screen flex flex-col pt-28" // Added padding-top to create space for the header
+      style={{ backgroundColor: theme.colors.bgPrimary }}
+    >
+      <Header />
+      <button
+        onClick={handleBackClick}
+        style={{
+          backgroundColor: theme.colors.bgAccent,
+          color: theme.colors.textPrimary,
+        }}
+      >
+        Back to Home
+      </button>
     </div>
   );
 };
